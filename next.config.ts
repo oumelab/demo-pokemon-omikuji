e592aaba2c8ec02a3419f01cc785d4a9000c6af0
Keiko Iwata <oumelab@pt.rdy.jp>
2025-04-19 17:36:06 +0900

    トップページの実装

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    // next.js v14 以前の記述の仕方。これでも動くが、非推奨。
+    // domains: ["raw.githubusercontent.com"],
+
+    // next.js v14 以降は、下記のような記述が推奨
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
